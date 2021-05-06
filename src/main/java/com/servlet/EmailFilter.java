b0252a2fc0c29401e8a8d0067ebd1c8dd49d6939
Sujatha <sujatha.deen@anywhere.co>
2021-05-06 16:31:27 +0530

    Added Session

--- src/main/java/com/servlet/EmailFilter.java
+++ src/main/java/com/servlet/EmailFilter.java
@@ -14,48 +14,47 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 
-
 //@WebFilter("/login")
-public class UserNameFilter implements Filter {
+public class EmailFilter implements Filter {
 
-	
+    
 	public void destroy() {
 		// TODO Auto-generated method stub
 	}
 
+	
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-		
+
 		HttpServletRequest httpRequest=(HttpServletRequest) request;
 		HttpServletResponse httpResponse=(HttpServletResponse) response;
 		
-		PrintWriter printWriter=response.getWriter();
+		PrintWriter printWriter = response.getWriter();
 		
 		HttpSession session = httpRequest.getSession();
 		
-		try
-		{
-		String email = request.getParameter("email");
-		String password = request.getParameter("password");
-		String userName = request.getParameter("userName");
+		try {
+		String email = (String) session.getAttribute("email");
+		String password=(String) session.getAttribute("password");
 		
-		session.setAttribute("userName", userName);
-		session.setAttribute("email", email);
-		session.setAttribute("password", password);
+		String regex = "^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$";
 		
-		String regex = "^[a-zA-Z_.]+$";
-		
-		if(userName.matches(regex)) {
+		if(email.matches(regex)){
+			if(password.equals("abc@123")) {
 			chain.doFilter(request, response);
+			}
+			
+			else {
+				response.getWriter().println("Invalid Password");
+			}
 		}
 		else {
-			response.getWriter().println("Username should be in Alphabetic");
+			response.getWriter().println("Invalid Email");
 		}
 		}
 		
 		catch(Exception e){
 			printWriter.println("Error : "+e.getMessage());
 		}
-		
 	}
 
 	
@@ -64,4 +63,3 @@ public class UserNameFilter implements Filter {
 	}
 
 }
-
--- src/main/java/com/servlet/LoginServlet.java
+++ src/main/java/com/servlet/LoginServlet.java
@@ -7,32 +7,46 @@ import jakarta.servlet.ServletConfig;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 
 public class LoginServlet extends HttpServlet {
 	
 	public void service(HttpServletRequest request, HttpServletResponse response) throws IOException {
+		response.setContentType("text/html");
+		
 		PrintWriter printWriter=response.getWriter();
+		
+		HttpSession session =request.getSession();
 		
 		try 
 		{
-		String userName = request.getParameter("userName");
-		String password = request.getParameter("password");
+		String userName = (String) session.getAttribute("userName");
+		String email = (String) session.getAttribute("email");
+		String password=(String) session.getAttribute("password");
 		
 		printWriter.println("User Name : "+userName);
-		printWriter.println("Password : "+password);
+		printWriter.println("<br>Email : "+email);
+		printWriter.println("<br>Password : "+password);
 		
 		ServletConfig config=getServletConfig();
-		printWriter.println("Name : "+config.getInitParameter("name"));
+		printWriter.println("<br>Name : "+config.getInitParameter("name"));
 		
-		printWriter.println("Login Successful");
+		printWriter.println("<br>Login Successful");
+		
+		session.invalidate();
+		//Try to check whether the session is invalidate or not
+		userName = (String) session.getAttribute("userName");
+		
 		}
 		
 		catch(Exception e) 
 		{
-			printWriter.println("Error : "+e.getMessage());
+			printWriter.println("<br><br>Error : "+e.getMessage());
 		}
 		
 	}
 
 
 }
+
+
